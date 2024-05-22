bfb7946d46c9f295642346a92074f5d98ebab3a0
Anikethsubhas <88712184+Anikethsubhas@users.noreply.github.com>
2024-05-23 02:16:14 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2')
 
 
-const con = mysql.createConnection({
+const con = mysql.createPool({
     host: "database-1.c1gko0s6qhw7.ap-south-1.rds.amazonaws.com",
     user: "admin",
     password: "12345678",
-    database: "UserInfo"
-})
+    database: "UserInfo",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 con.connect(function(err) {
     if (err) {
